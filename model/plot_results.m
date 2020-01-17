8177354cf47dd99ca63650ec2ef56340e70bcbd0
jason-mav <jason-mav@hotmail.com>
2020-01-17 22:43:07 +0200

    0

--- model/plot_results.m
+++ model/plot_results.m
@@ -26,9 +26,9 @@ else
     active_days = i-j;
 end
 
-path = sprintf('D:\\Users\\Jay\\Google Drive\\???????????\\???????????\\Reproduce paper\\ch 3 [2010] Banks\\periodic dose\\case %d\\[%d-%d]', case_sel, period, active_days);
-cells_print_path = sprintf('D:\\Users\\Jay\\Google Drive\\???????????\\???????????\\Reproduce paper\\ch 3 [2010] Banks\\periodic dose\\case %d\\[%d-%d].bmp', case_sel, period, active_days);
-drug_print_path = sprintf('D:\\Users\\Jay\\Google Drive\\???????????\\???????????\\Reproduce paper\\ch 3 [2010] Banks\\periodic dose\\case %d\\[%d-%d]-d.bmp', case_sel, period, active_days);
+path = sprintf('figures\\case %d\\[%d-%d]', case_sel, period, active_days);
+print_path = sprintf('figures\\case %d\\[%d-%d].png', case_sel, period, active_days);
+drug_print_path = sprintf('figures\\case %d\\[%d-%d]-d.png', case_sel, period, active_days);
 
 % Cells figure
 fig_cells = figure(1);
@@ -38,13 +38,20 @@ title(sprintf('Period/Active days: [%d/%d]', period, active_days),'fontsize',12)
 set(gca,'FontSize',11)
 xlabel('Days','fontsize',12)
 ylabel('Cells','fontsize',12)
+legend('N','T','I','M')
 
 saveas(fig_cells, path, 'fig');
-print(fig_cells,'-dbmp',cells_print_path);
+print(fig_cells,'-dpng',print_path);
 
 % Drug figure
-fig_drug = figure(2)
+fig_drug = figure(2);
 stairs(Drug_out.time,Drug_out.data,'k','LineWidth',1)
-ylim([0 1.2])
-print(fig_drug,'-dbmp',drug_print_path);
+
+title(sprintf('Drug input - Period/Active days: [%d/%d]', period, active_days),'fontsize',12)
+set(gca,'FontSize',11)
+xlabel('Days','fontsize',12)
+ylabel('Drug','fontsize',12)
+
+saveas(fig_drug, strcat(path,'-d'), 'fig');
+print(fig_drug,'-dpng', drug_print_path);
 
